fd4eb7ddceeb5df713c77796c7c057ed7e8566fd
Harshith S <87616210+Harshith5S@users.noreply.github.com>
2021-07-24 14:26:04 +0530

    ttms

---
+++ ttms/ttms/allotmentpracticalFormvalidation.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MSaqib
+ * Date: 17-11-2016
+ * Time: 19:22
+ */
+
+include 'connection.php';
+if (isset($_POST['tobealloted'])) {
+    $subject = $_POST['tobealloted'];
+    $teacher = $_POST['toalloted'];
+    //  $message = "nTry again.";
+    // echo "<script type='text/javascript'>alert('$message');</script>";
+}
+$q = mysqli_query(mysqli_connect("localhost", "root", "", "ttms"), "UPDATE subjects SET isAlloted=1, allotedto='$teacher' WHERE subject_code='$subject'");
+
+if ($q) {
+    $message = "Done.\\nTry again.";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header("Location:allotpracticals.php");
+} else {
+    $message = "Username and/or Password incorrect.\\nTry again.";
+    $message = $subject;
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    // header("Location:index.html");
+
+}
+
+?>
